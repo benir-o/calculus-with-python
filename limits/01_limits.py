f4710992111129716311e52fb12ec95d0d6266b4
benir-o <beniromenda@gmail.com>
2025-05-09 00:04:37 +0300

    Behavior of our graph as x approaches 2

--- limits/01_limits.py
+++ limits/01_limits.py
@@ -1,1 +1,11 @@
 print("This will be used to calculate limits")
+x=2
+h= 0.00001
+y_right= (3*((x+h)-2)/((x+h) -2))
+y_left=(3*((x-h)-2))/((x-h)-2)
+
+print("y_right: ",y_right)
+print("y_left: ",y_left)
+
+if round(y_right) != round(y_left):
+    print("Limit does not exist at x=",x)
